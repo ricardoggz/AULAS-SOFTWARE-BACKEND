bb56212b4220c772658c51ac2b239cf37c2447b1
himfg <himfgence@gmail.com>
2024-09-17 09:50:49 -0600

    configuracion inicial del proyecto

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+//Se ejecuta express
+const server = express()
+
+//El servidor estaá corriendo en el puerto 3030 (localhost:3030)
+server.listen(3030)
+
+//Rutas (endpoints) - Ejemplo: 'localhost:3030/eventos'
+//http: Hyper Text Trnasfer Protocol
+//https: Hyper Text Trnasfer Protocol - SSL
+//get: recibe información del servidor
+//post: Se manda información al servidor
+//put: Actualizar información del servidor
+//delete: eliminar información del servidor
+server.get('/', (req, res)=>{
+    res.send('Servidor Activo')
+})
+server.get('/eventos', (req, res)=>{
+    res.json('Eventos')
+})
